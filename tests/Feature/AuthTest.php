4364d7ff57de9b0aeca08c271e0c30449a15dc37
m4th3usfp <matheuspizzinato975@gmail.com>
2025-05-29 18:55:30 -0300

    começando os testes

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -18,13 +18,15 @@ class AuthTest extends TestCase
     {
         // Cria a cidade "Frutal"
         $cidade = City::factory()->create(['name' => 'Frutal']);
-    
+        // dd($cidade->name);
         // Cria um usuário vinculado à cidade
         $user = User::factory()->create([
             'name' => 'Matheus',
-            'city' => 'Frutal',
+            'city' => $cidade->name,
+            'city_id' => $cidade->id,
             'password' => Hash::make('fanuchy98'),
         ]);
+        // dd($cidade, $user);
 
         // Envia o formulário de login (não precisa passar _token em testes)
         $response = $this->post('/login', [
@@ -38,5 +40,34 @@ class AuthTest extends TestCase
 
         // Verifica se o usuário está autenticado
         $this->assertAuthenticatedAs($user);
+    }
+
+    
+    public function test_usuario_com_credenciais_invalidas_nao_loga()
+    {
+        $cidade = City::factory()->create(['name' => 'Frutal']);
+        
+        $user = User::factory()->create([
+            'name' => 'Matheus',
+            'city' => $cidade->name,
+            'city_id' => $cidade->id,
+            'password' => Hash::make('fanuchy98'),
+        ]);
+        
+        $wrong_user = [
+            'name' => 'Rodrigo',
+            'city' => 'Araras',
+            'city_id' => '1',
+            'password' => Hash::make('fanuchy98'),
+        ];
+        
+        $response = $this->post('/login', [$wrong_user]);
+
+        $response->assertRedirect();
+
+        $response->assertSessionHasErrors();
+
+        $this->assertGuest();
+        
     }
 }
