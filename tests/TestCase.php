2298632ae2cafb557fa980b3756b8f0bf5409ed2
m4th3usfp <matheuspizzinato975@gmail.com>
2025-05-28 22:49:16 -0300

    ambiente configurado, AuthTest falhando ainda

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,15 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        // dd('env APP_ENV = ' . env('APP_ENV'));
+        // dd('config app.env = ' . config('app.env'));
+
+        if (config('app.env') !== 'testing') {
+            throw new \Exception('Testes devem ser rodados apenas no ambiente de teste!');
+        }
+    }
 }
