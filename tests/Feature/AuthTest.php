c18863b410918401464e149e078588b923896dd1
m4th3usfp <matheuspizzinato975@gmail.com>
2025-06-02 19:20:55 -0300

    mais 2 testes feitos

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\City;
+use App\Models\Fisherman;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Hash;
@@ -70,4 +71,66 @@ class AuthTest extends TestCase
         $this->assertGuest();
         
     }
+
+    public function test_acessar_rota_cadastro()
+    {
+        $cidade = City::factory()->create(['name' => 'Frutal']);
+
+        // dd($cidade);
+
+        $user = User::factory()->create([
+            'name' => 'Matheus',
+            'city' => $cidade->name,
+            'city_id' => $cidade->id,
+            'password' => Hash::make('fanuchy98'), 
+        ]);
+
+        $this->actingAs($user);
+
+        $response = $this->get('/Cadastro');
+
+        $response->assertOk();
+
+        $response->assertViewIs('Cadastro');
+
+    }
+
+    public function test_usuario_autenticado_faz_post()
+    {
+        // Cria a cidade e o usuário
+        $cidade = City::factory()->create(['name' => 'Frutal']);
+        $record_number = (int) Fisherman::max('record_number');
+        $last_record_number = $record_number + 1;
+        $user = User::factory()->create([
+            'name' => 'Matheus',
+            'city' => $cidade->name,
+            'city_id' => $cidade->id,
+            'password' => Hash::make('fanuchy98'),
+        ]);
+    
+        // Autentica como esse usuário
+        $this->actingAs($user);
+    
+        // Dados para criar um novo pescador
+        $dadosPescador = [
+            'record_number' => $last_record_number,
+            'name' => 'João Pescador',
+            'email' => 'joao@hotmail.com',
+            'city_id' => $cidade->id,
+        ];
+    
+        // Envia requisição POST
+        $response = $this->post('/Cadastro', $dadosPescador);
+        // Verifica se foi redirecionado corretamente (ajuste conforme o comportamento da sua aplicação)
+        $response->assertRedirect('/listagem');
+        
+        // Verifica se o pescador foi criado no banco
+        $this->assertDatabaseHas('fishermen', [
+            'name' => 'João Pescador',
+            'email' => 'joao@hotmail.com',
+            'record_number' => $las
+        ]);
+    }
 }
+    
+
