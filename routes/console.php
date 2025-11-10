4c637e9f10cdddc064f146fb300937f3384de92a
m4th3usfp <matheuspizzinato975@gmail.com>
2025-11-10 09:19:11 -0300

    teste schedule as 9:21h

--- routes/console.php
+++ routes/console.php
@@ -11,4 +11,4 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 // ✅ Agendamento diário do comando de backup às 19h
-Schedule::command('app:backup')->dailyAt('09:17');
+Schedule::command('app:backup')->dailyAt('09:21');
