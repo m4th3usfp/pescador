b712ac137ffc8c54c14d0da91b72a31fc5ceae9d
m4th3usfp <matheuspizzinato975@gmail.com>
2025-11-21 12:31:37 -0300

    edição diminuir os campos do cadastro, imitar o antigo

--- resources/views/Cadastro.blade.php
+++ resources/views/Cadastro.blade.php
@@ -107,146 +107,146 @@
                 </div>
 
                 @endif
-                <form class="mt-2" method="POST" action="{{ isset($cliente) ? route('pescadores.update', $cliente->id) : route('store') }}">
-                    <div class="container col-12">
-                        <div class="col-md-12 mb-2 mb-1">
-                            <label for="record_number" class="form-label mb-1">Ficha</label>
+                <form class="" method="POST" action="{{ isset($cliente) ? route('pescadores.update', $cliente->id) : route('store') }}">
+                    <div class="container col-12 mt-4">
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="record_number" class="form-label mb-1 me-1">Ficha:</label>
                             <input type="text" class="form-control borda-grossa" id="record_number" name="record_number" value="{{ $recordNumber }}" readonly>
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="name" class="form-label mb-1">nome</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="name" class="form-label mb-1 me-1">nome:</label>
                             <input type="text" class="form-control borda-grossa" id="name" name="name" value="{{ $cliente->name ?? '' }}" required>
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="father_name" class="form-label mb-1">Nome do Pai</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="father_name" class="form-label mb-1 me-1">Nome do Pai:</label>
                             <input type="text" class="form-control borda-grossa" id="father_name" name="father_name" value="{{ $cliente->father_name ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="mother_name" class="form-label mb-1">Nome da Mãe</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="mother_name" class="form-label mb-1 me-1">Nome da Mãe:</label>
                             <input type="text" class="form-control borda-grossa" id="mother_name" name="mother_name" value="{{ $cliente->mother_name ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="address" class="form-label mb-1">Endereço</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="address" class="form-label mb-1 me-1">Endereço:</label>
                             <input type="text" class="form-control borda-grossa" id="address" name="address" value="{{ $cliente->address ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="city" class="form-label mb-1">Cidade</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="city" class="form-label mb-1 me-1">Cidade</label>
                             <input type="text" class="form-control borda-grossa" id="city" name="city" value="{{ $cliente->city ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="state" class="form-label mb-1">Estado</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="state" class="form-label mb-1 me-1">Estado:</label>
                             <input type="text" class="form-control borda-grossa" id="state" name="state" value="{{ $cliente->state ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="house_number" class="form-label mb-1">Número</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="house_number" class="form-label mb-1 me-1">Número:</label>
                             <input type="text" class="form-control borda-grossa" id="house_number" name="house_number" value="{{ $cliente->house_number ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="neighborhood" class="form-label mb-1">Bairro</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="neighborhood" class="form-label mb-1 me-1">Bairro:</label>
                             <input type="text" class="form-control borda-grossa" id="neighborhood" name="neighborhood" value="{{ $cliente->neighborhood ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="zip_code" class="form-label mb-1">CEP</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="zip_code" class="form-label mb-1 me-1">CEP:</label>
                             <input type="text" class="form-control borda-grossa" id="zip_code" name="zip_code" value="{{ $cliente->zip_code ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="mobile_phone" class="form-label mb-1">Celular</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="mobile_phone" class="form-label mb-1 me-1">Celular:</label>
                             <input type="text" class="form-control borda-grossa" id="mobile_phone" name="mobile_phone" value="{{ $cliente->mobile_phone ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="phone" class="form-label mb-1">Telefone</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="phone" class="form-label mb-1 me-1">Telefone:</label>
                             <input type="text" class="form-control borda-grossa" id="phone" name="phone" value="{{ $cliente->phone ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="secondary_phone" class="form-label mb-1">Telefone para Recado</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="secondary_phone" class="form-label mb-1 me-1">Telefone para Recado:</label>
                             <input type="text" class="form-control borda-grossa" id="secondary_phone" name="secondary_phone" value="{{ $cliente->secondary_phone ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="marital_status" class="form-label mb-1">Estado Civil</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="marital_status" class="form-label mb-1 me-1">Estado Civil:</label>
                             <input type="text" class="form-control borda-grossa" id="marital_status" name="marital_status" value="{{ $cliente->marital_status ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="profession" class="form-label mb-1">Profissão</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="profession" class="form-label mb-1 me-1">Profissão:</label>
                             <input type="text" class="form-control borda-grossa" id="profession" name="profession" value="{{ $cliente->profession ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="tax_id" class="form-label mb-1">CPF</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="tax_id" class="form-label mb-1 me-1">CPF:</label>
                             <input type="text" class="form-control borda-grossa" id="tax_id" name="tax_id" value="{{ $cliente->tax_id ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="identity_card" class="form-label mb-1">RG</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="identity_card" class="form-label mb-1 me-1">RG:</label>
                             <input type="text" class="form-control borda-grossa" id="identity_card" name="identity_card" value="{{ $cliente->identity_card ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="identity_card_issuer" class="form-label mb-1">Orgão emissor do RG</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="identity_card_issuer" class="form-label mb-1 me-1">Orgão emissor do RG:</label>
                             <input type="text" class="form-control borda-grossa" id="identity_card_issuer" name="identity_card_issuer" value="{{ $cliente->identity_card_issuer ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="identity_card_issue_date" class="form-label mb-1">Data da Emissão do RG</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="identity_card_issue_date" class="form-label mb-1 me-1">Data da Emissão do RG:</label>
                             <input type="text" class="form-control borda-grossa" id="identity_card_issue_date" name="identity_card_issue_date" value="{{ $cliente->identity_card_issue_date ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="voter_id" class="form-label mb-1">Título de Eleitor</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="voter_id" class="form-label mb-1 me-1">Título de Eleitor:</label>
                             <input type="text" class="form-control borda-grossa" id="voter_id" name="voter_id" value="{{ $cliente->voter_id ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="work_card" class="form-label mb-1">Carteira de Trabalho</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="work_card" class="form-label mb-1 me-1">Carteira de Trabalho:</label>
                             <input type="text" class="form-control borda-grossa" id="work_card" name="work_card" value="{{ $cliente->work_card ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="rgp" class="form-label mb-1">RGP</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="rgp" class="form-label mb-1 me-1">RGP:</label>
                             <input type="text" class="form-control borda-grossa" id="rgp" name="rgp" value="{{ $cliente->rgp ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="rgp_issue_date" class="form-label mb-1">Data da RGP</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="rgp_issue_date" class="form-label mb-1 me-1">Data da RGP:</label>
                             <input type="text" class="form-control borda-grossa" id="rgp_issue_date" name="rgp_issue_date" value="{{ $cliente->rgp_issue_date ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="pis" class="form-label mb-1">PIS</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="pis" class="form-label mb-1 me-1">PIS:</label>
                             <input type="text" class="form-control borda-grossa" id="pis" name="pis" value="{{ $cliente->pis ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="cei" class="form-label mb-1">CEI</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="cei" class="form-label mb-1 me-1">CEI:</label>
                             <input type="text" class="form-control borda-grossa" id="cei" name="cei" value="{{ $cliente->cei ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="drivers_license" class="form-label mb-1">CNH</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="drivers_license" class="form-label mb-1 me-1">CNH:</label>
                             <input type="text" class="form-control borda-grossa" id="drivers_license" name="drivers_license" value="{{ $cliente->drivers_license ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="license_issue_date" class="form-label mb-1">Data da emissão da CNH</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="license_issue_date" class="form-label mb-1 me-1">Data da emissão da CNH:</label>
                             <input type="text" class="form-control borda-grossa" id="license_issue_date" name="license_issue_date" value="{{ $cliente->license_issue_date ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="email" class="form-label mb-1">Email</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="email" class="form-label mb-1 me-1">Email:</label>
                             <input type="email" class="form-control borda-grossa" id="email" name="email" value="{{ $cliente->email ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="affiliation" class="form-label mb-1">Filiação</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="affiliation" class="form-label mb-1 me-1">Filiação:</label>
                             <input type="text" class="form-control borda-grossa" id="affiliation" name="affiliation" value="{{ $cliente->affiliation ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="birth_date" class="form-label mb-1">Nascimento</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="birth_date" class="form-label mb-1 me-1">Nascimento:</label>
                             <input type="text" class="form-control borda-grossa" id="birth_date" name="birth_date" value="{{ $cliente->birth_date ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="birth_place" class="form-label mb-1">Local de nascimento</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="birth_place" class="form-label mb-1 me-1">Local de nascimento:</label>
                             <input type="text" class="form-control borda-grossa" id="birth_place" name="birth_place" value="{{ $cliente->birth_place ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="expiration_date" class="form-label mb-1">Vencimento</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="expiration_date" class="form-label mb-1 me-1">Vencimento:</label>
                             <input type="text" class="form-control borda-grossa" id="expiration_date" name="expiration_date" value="{{ $cliente->expiration_date ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="notes" class="form-label mb-1">Senha</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="notes" class="form-label mb-1 me-1">Senha:</label>
                             <input type="text" class="form-control borda-grossa" id="notes" name="notes" value="{{ $cliente->notes ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="caepf_code" class="form-label mb-1">Código de Acesso CAEPF</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="caepf_code" class="form-label mb-1 me-1">Código de Acesso CAEPF:</label>
                             <input type="text" class="form-control borda-grossa" id="caepf_code" name="caepf_code" value="{{ $cliente->caepf_code ?? '' }}">
                         </div>
-                        <div class="col-md-12 mb-2">
-                            <label for="caepf_password" class="form-label mb-1">Senha CAEPF</label>
+                        <div class="col-md-12 mb-1 d-flex">
+                            <label for="caepf_password" class="form-label mb-1 me-1">Senha CAEPF:</label>
                             <input type="password" class="form-control borda-grossa" id="caepf_password" name="caepf_password" value="{{ $cliente->caepf_password ?? '' }}">
                         </div>
                         
