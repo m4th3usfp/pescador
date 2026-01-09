40cef63beb12afecf3759382a15ed1f596bdeca7
m4th3usfp <matheuspizzinato975@gmail.com>
2026-01-09 18:09:53 -0300

    deletou daniely adicionou Leandro teste5

--- resources/views/activity_log_table.blade.php
+++ resources/views/activity_log_table.blade.php
@@ -51,111 +51,17 @@
 
 
                 @foreach ($logs as $log)
-
-                @php
-                $isExpired = false;
-
-                if (!empty($log->properties['Vencimento'])) {
-                try {
-                $date = \Carbon\Carbon::parse($log->properties['Vencimento']);
-
-                // Só marca como vencido se for realmente no passado
-                $isExpired = $date->isPast();
-
-                } catch (\Exception $e) {
-
-                // Se a data for inválida, NÃO marca como vencido
-                $isExpired = false;
-
-                }
-                }
-                $novo = $log->properties['Novo'] ?? [];
-                $antigo = $log->properties['Antigo'] ?? [];
-
-                $fieldLabels = [
-
-                'name' => 'Nome',
-                'address' => 'Endereço',
-                'house_number' => 'Numero',
-                'neighborhood' => 'Bairro',
-                'city' => 'Cidade',
-                'state' => 'Estado',
-                'zip_code' => 'CEP',
-                'mobile_phone' => 'Celular',
-                'phone' => 'Telefone',
-                'secondary_phone' => 'Telefone_secundario',
-                'tax_id' => 'CPF',
-                'identity_card' => 'RG',
-                'identity_card_issuer' => 'Emissor',
-                'rgp' => 'RGP',
-                'pis' => 'PIS',
-                'cei' => 'CEI',
-                'drivers_license' => 'CNH',
-                'license_issue_date' => 'Data CNH',
-                'email' => 'Email',
-                'expiration_date' => 'Vencimento',
-                'affiliation' => 'Filiação',
-                'birth_date' => 'Data nascimento',
-                'birth_place' => 'Local nascimento',
-                'notes' => 'Senha',
-                'identity_card_issue_date' => 'Data RG',
-                'father_name' => 'Nome pai',
-                'mother_name' => 'Nome mãe',
-                'rgp_issue_date' => 'Data RGP',
-                'voter_id' => 'Carteira eleitor',
-                'work_card' => 'Carteira trabalho',
-                'profession' => 'Profissão',
-                'marital_status' => 'Estado civil',
-
-                ];
-
-                // Monta texto "Campo: Valor"
-                $formatCampos = function ($dados, $labels) {
-                $resultado = [];
-
-                foreach ($dados as $campo => $valor) {
-                $nomeCampo = $labels[$campo] ?? $campo;
-                $valor = formatIfDateValue($valor);
-
-                $resultado[] = "<strong>{$nomeCampo}:</strong> {$valor}";
-                }
-
-                return $resultado;
-                };
-
-                $formatadoFinal = [];
-
-                foreach ($fieldLabels as $campo => $label) {
-
-                $valorNovo = $novo[$campo] ?? null;
-                $valorAntigo = $antigo[$campo] ?? null;
-
-                if ($valorNovo !== null || $valorAntigo !== null) {
-
-                $valorNovo = formatIfDateValue($valorNovo);
-                $valorAntigo = formatIfDateValue($valorAntigo);
-
-                $formatadoFinal[] = "
-                <div class='alteracao-box'>
-                    <div class='alteracao-titulo'>{$label}:</div>
-                    <div class='alteracao-novo nowrap'><strong>Novo:</strong> {$valorNovo}</div>
-                    <div class='alteracao-antigo nowrap'><strong>Antigo:</strong> {$valorAntigo}</div>
-                </div>
-                ";
-                }
-                }
-                @endphp
                 <tr>
                     <td>{{ \Carbon\Carbon::parse($log->created_at)->format('d/m/Y H:i:s') }}</td>
                     <td>{{ $log->properties['Usuario'] ?? '----' }}</td>
                     <td>{{ $log->log_name }}</td>
-                    <td style="color: {{ $isExpired ? '#C00000' : 'black' }}">
+                    <td>
                         {{ $log->properties['Pescador_nome'] ?? '----' }}
                     </td>
                     <td>{{ $log->description ?? '----' }}</td>
                     <td>
                         <div class="alteracoes-grid">
-                            {!! implode('', $formatadoFinal) !!}
+                            
                         </div>
                     </td>
                 </tr>
